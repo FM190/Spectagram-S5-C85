b176f95f345f2d4199cc8dc9efdd870085008d5e
FM190 <86798311+FM190@users.noreply.github.com>
2021-11-01 20:39:48 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAEE3On1ynfc3UkFGC1HUExDxDfhcPR50w",
+  authDomain: "story-app-b43bc.firebaseapp.com",
+  projectId: "story-app-b43bc",
+  storageBucket: "story-app-b43bc.appspot.com",
+  messagingSenderId: "120713238664",
+  appId: "1:120713238664:web:987afb6a824a705ca5e73d"
+};
